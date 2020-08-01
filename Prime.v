d1de10b427fba51066a0c9331dbcd9ccc69df59b
volodeyka <vovaglad00@gmail.com>
2020-08-01 21:20:23 +0300

    prime event structures and sipmle properties
    some improves
    
    ltac improved

--- Prime.v
+++ Prime.v
@@ -7,9 +7,10 @@ Declare Scope hb_scope.
 Delimit Scope hb_scope with G.
 Open Scope hb_scope.
 
-Notation sw := (ltac:(move=> A B; move: B A)).
+(*Notation sw := 
+  (ltac:(let f := fresh "_top_" in let s := fresh "_s_" in move=> f s; move: f s)).*)
 
-Definition left_clo {T : Type} (r : rel T) y := fun x => r x y.
+Definition left_clo {T : Type} (r : rel T) y := r ^~ y.
 
 
 Definition finite {T : Type} (a : pred T) := 
@@ -28,12 +29,11 @@ Notation "a <= b" := (le a b) : hb_scope.
 
 
 Section event_struct.
-Context (E : evstruct.type).
-Implicit Types (p : pred E) (e : E) (s : seq E).
+Context {E : evstruct.type}.
+Implicit Types (p : pred E) (e : E).
 
 
 Definition left_closed p := forall e1 e2, e1 \in p -> e2 <= e1 -> e2 \in p.
-Definition finite_causes_axiom := forall e, finite (left_clo le e).
 
 Lemma left_cloE e1 e2: (e2 \in left_clo le e1) = (e2 <= e1).
 Proof. by []. Qed.
@@ -43,31 +43,31 @@ Proof. move=> ??? /le_trans. by apply. Qed.
 
 End event_struct.
 
+
 HB.mixin Record prime_of_event_struct E of evstruct E := 
 {
     confl           : rel E;
     finite_left_clo : forall e : E, finite (left_clo le e); (***)
     confl_irrefl    : irreflexive confl;
     symm_confl      : symmetric confl;
-    consist_confl   : forall e1 e2 e3 : E, (confl e1 e2) = true -> (le e2 e3) = true -> (confl e1 e3) = true; (***)
+    consist_confl   : forall e2 e1 e3 : E, (confl e1 e2) = true -> (le e2 e3) = true -> (confl e1 e3) = true; (***)
 }.
 
 HB.structure Definition prime := { T of evstruct T & prime_of_event_struct T }.
 Notation "a # b" := (confl a b) (at level 0) : hb_scope.
 
 Section prime_event_struct.
-Variables (E : prime.type).
+Context {E : prime.type}.
 Implicit Types (p : pred E) (e : E) (s : seq E).
 
 Definition confl_free p := forall e1 e2, e1 \in p -> e2 \in p -> ~~ (e1 # e2).
-Definition config p := (confl_free p * left_closed E p)%type.
+Definition config p := (confl_free p * left_closed p)%type.
 
 Lemma config_left_clo e: config (left_clo le e).
 Proof.
-split=> [e1 e2|]; last by apply left_closed_left_clo. 
-case confl: (e1 # e2)=> //= /sw/(consist_confl _ _ _ confl).
-rewrite symm_confl=> confl' /(consist_confl _ _ _ confl').
-by rewrite confl_irrefl.
+split=> [e1 e2 *|]; last by apply: left_closed_left_clo.
+case confl: (e1 # e2)=> //=. rewrite -(confl_irrefl e).
+by rewrite (consist_confl e2)// symm_confl (consist_confl e1)// symm_confl.    
 Qed.
 
 End prime_event_struct.
