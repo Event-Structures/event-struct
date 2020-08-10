f79f3c8e029dadb25ca70ce6c6bb05d9d49729c4
volodeyka <vovaglad00@gmail.com>
2020-08-10 19:59:35 +0300

    abstract intrface of event strcutures added

--- EventStructure.v
+++ EventStructure.v
@@ -1,4 +1,6 @@
-From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat seq fintype order eqtype div fingraph choice.
+From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat seq fintype eqtype div fingraph choice order.
+Import Order.LTheory.
+Open Scope order_scope.
 
 Lemma ltn_modS a b: (a %% (b.+1) < b.+1)%N.
 Proof. by rewrite ltn_pmod. Qed.
@@ -50,21 +52,20 @@ Arguments r {_}.
 Module EventStructure.
 
 Structure mixin_of {val : Type} (T : choiceType) := Mixin {
-  E                   : T -> (ev_label val);
-  cause               : rel T; (* seq (T * T)%type *)
-  conflict            : rel T; (* seq (T * T)%type *)
-  rf                  : T -> option T;
-  rf_codom            : forall k, reflect (is_write (E k)) (rf k == None);
-  rf_dom              : forall k, is_read (E k) <-> (exists m, (rf k = some m) /\ is_write (E m));
-  rf_correct          : forall k l x i, rf k = some l -> E k = R x i -> E l = (W x i);
-  refl_cause          : reflexive     cause;
-  anti_cause          : antisymmetric cause;
-  trans_cause         : transitive    cause;
-  finite_causes_axiom : finite_downward_closure cause;
-  confl_irrefl        : irreflexive conflict;
-  symm_confl          : symmetric   conflict;
-  consist_confl       : forall e1 e2 e3, 
-                        conflict e2 e1 -> cause e1 e3 -> conflict e2 e3;
+  E        : T -> (ev_label val);
+  cause    : rel T; (* seq (T * T)%type *)
+  conflict : rel T; (* seq (T * T)%type *)
+  rf       : T -> option T;
+  _        : forall k, reflect (is_write (E k)) (rf k == None);
+  _        : forall k, is_read (E k) <-> (exists m, (rf k = some m) /\ is_write (E m));
+  _        : forall k l x i, rf k = some l -> E k = R x i -> E l = (W x i);
+  _        : reflexive     cause;
+  _        : antisymmetric cause;
+  _        : transitive    cause;
+  _        : finite_downward_closure cause;
+  _        : irreflexive conflict;
+  _        : symmetric   conflict;
+  _        : forall e1 e2 e3, conflict e2 e1 -> cause e1 e3 -> conflict e2 e3;
 }.
 
 Notation class_of := mixin_of (only parsing).
@@ -92,24 +93,54 @@ End EventStructure.
 
 Export EventStructure.Exports.
 
-Section Definitions.
+Section LemmasExport.
 Context {val : Type} {T : eventType val}.
-Definition E                   := EventStructure.E                   T (EventStructure.class T).
-Definition cause               := EventStructure.cause               T (EventStructure.class T).
-Definition conflict            := EventStructure.conflict            T (EventStructure.class T).
-Definition rf                  := EventStructure.rf                  T (EventStructure.class T).
-Definition rf_codom            := EventStructure.rf_codom            T (EventStructure.class T).
-Definition rf_dom              := EventStructure.rf_dom              T (EventStructure.class T).
-Definition rf_correct          := EventStructure.rf_correct          T (EventStructure.class T).
-Definition refl_cause          := EventStructure.refl_cause          T (EventStructure.class T).
-Definition anti_cause          := EventStructure.anti_cause          T (EventStructure.class T).
-Definition trans_cause         := EventStructure.trans_cause         T (EventStructure.class T).
-Definition finite_causes_axiom := EventStructure.finite_causes_axiom T (EventStructure.class T).
-Definition confl_irrefl        := EventStructure.confl_irrefl        T (EventStructure.class T).
-Definition symm_confl          := EventStructure.symm_confl          T (EventStructure.class T).
-Definition consist_confl       := EventStructure.consist_confl       T (EventStructure.class T).
-End Definitions.
+Definition E := EventStructure.E T (EventStructure.class T).
+Definition cause := EventStructure.cause T (EventStructure.class T).
+Definition conflict := EventStructure.conflict T (EventStructure.class T).
+Definition rf := EventStructure.rf T (EventStructure.class T).
 
+Lemma trans_cause : transitive cause.
+Proof. rewrite /cause. by case: T => ? []. Qed.
+Lemma anti_cause : antisymmetric cause.
+Proof. rewrite /cause. by case: T => ? []. Qed.
+Lemma refl_cause: reflexive cause.
+Proof. rewrite /cause. by case: T => ? []. Qed.
+Lemma rf_codom: forall k, reflect (is_write (E k)) (rf k == None).
+Proof. rewrite /E/rf. by case: T => ? []. Qed.
+Lemma rf_dom: forall k, is_read (E k) <-> (exists m, (rf k = some m) /\ is_write (E m)).
+Proof. rewrite /E/rf. by case: T => ? []. Qed.
+Lemma rf_correct: forall k l x i, rf k = some l -> E k = R x i -> E l = (W x i).
+Proof. rewrite /E/rf. by case: T => ? []. Qed.
+Lemma finite_causes_axiom: finite_downward_closure cause.
+Proof. rewrite /cause. by case: T => ? []. Qed.
+Lemma confl_irrefl: irreflexive conflict.
+Proof. rewrite /conflict. by case: T => ? []. Qed.
+Lemma symm_confl: symmetric conflict.
+Proof. rewrite /conflict. by case: T=> ? []. Qed.
+Lemma consist_confl: forall e1 e2 e3, conflict e2 e1 -> cause e1 e3 -> conflict e2 e3.
+Proof. rewrite /conflict/cause. by case: T => ? []. Qed.
+End LemmasExport.
+Notation "a # b" := (conflict a b) (at level 0).
+
+(*Section ordered.
+Context {val : Type} {T : eventType val}.
+Implicit Type x y z : T.
+
+Definition lt_of_cause x y := (y != x) && (cause x y).
+
+Lemma lt_neq_le : forall x y, lt_of_cause x y = (y != x) && (cause x y).
+Proof. done. Qed.
+
+Definition orderMixin :=
+ LePOrderMixin lt_neq_le refl_cause anti_cause trans_cause.
+
+Definition ev_display : unit.
+Proof. exact: tt. Qed.
+
+Canonical porderType := POrderType ev_display T orderMixin.
+
+End ordered.*)
 
 Section nat_event_structure.
 Context {val : Type}.
@@ -132,40 +163,59 @@ Structure evstruct := Pack {
   confl_irrefln        : irreflexive conflictn;
   symm_confln          : symmetric   conflictn;
   consist_confln       : forall e1 e2 e3, 
-                        conflictn e2 e1 -> causen e1 e3 -> conflictn e2 e3;
+                         conflictn e2 e1 -> causen e1 e3 -> conflictn e2 e3;
 }.
 
+Arguments EvMixin {_ _}.
+
 (* event structure configuration *)
+Canonical ev_Mixin (e : evstruct) := EvMixin
+  e.(En)
+  e.(causen)
+  e.(conflictn)
+  e.(rfn)
+  e.(rfn_codom)
+  e.(rfn_dom)
+  e.(rfn_correct)
+  e.(refl_causen)
+  e.(anti_causen)
+  e.(trans_causen)
+  e.(finite_causes_axiomn)
+  e.(confl_irrefln)
+  e.(symm_confln)
+  e.(consist_confln).
+Print EventStructure.Pack.
+Canonical ev_evType e := Eval hnf in EvType _ _ (ev_Mixin e).
 
 Notation "a '<=[' e ] b" := (e.(causen) a b) (at level 0).
 Notation "a '#[' e ] b" := (e.(conflictn) a b) (at level 0).
 
-(*Lemma dw_cl_dw_clo es l : downward_closed (downward_closure l es.(cause)) es.(cause).
-Proof.  move=> ??? /es.(trans_cause). by apply. Qed.
+Lemma dw_cl_dw_clo (es : evstruct) (l : 'I_es.(n).+1) : downward_closed (downward_closure l cause) cause.
+Proof.  move=> ??? /trans_cause. by apply. Qed.
 
 Definition confl_free es (p : pred 'I_es.(n).+1) := forall e1 e2, e1 \in p -> e2 \in p -> ~~ (e1 #[es] e2).
 Definition config es (p : pred 'I_es.(n).+1) :=
-  (confl_free es p * downward_closed p es.(cause))%type.
+  (confl_free es p * downward_closed p cause)%type.
 
-Lemma config_left_clo es e: config es (downward_closure e es.(cause)).
+Lemma config_left_clo es e: config es (downward_closure e cause).
 Proof.
-split=> [e1 e2 *|]; last by apply: dw_cl_dw_clo. case confl: (e1 #[es] e2)=> //=.
-rewrite -(es.(confl_irrefl) e) (es.(consist_confl) e2)// es.(symm_confl).
-by rewrite (es.(consist_confl) e1)// es.(symm_confl). 
+split=> [e1 e2 *|]; last by apply: dw_cl_dw_clo.
+case confl: (e1 #[es] e2)=> //=. rewrite -(confl_irrefl e).
+by rewrite (consist_confl e2)// symm_confl (consist_confl e1)// symm_confl. 
 Qed.
 
 (* transition system on event structures *)
 Definition evstruct_rel es es' lab (place : nat) :=
-  (es.(n) = es'.(n).+1)                                              /\
-  (forall n m, (n <=[es] m) -> $n <=[es'] $m)                        /\
-  (forall n m, n #[es] m -> $n #[es'] $m)                            /\
-  (forall n, es.(E) n = es'.(E) $n)                                  /\
-  (forall k m, ((es.(rf) k) == some m) = ((es'.(rf) $k) == some $m)) /\
-  (if lab is w x i then es'.(E) $(es'.(n)) = W x i
-   else is_read (es'.(E) $(es'.(n))))                                /\
+  (es.(n) = es'.(n).+1)                                                /\
+  (forall n m, (n <=[es] m) -> $n <=[es'] $m)                          /\
+  (forall n m, n #[es] m -> $n #[es'] $m)                              /\
+  (forall n, es.(En) n = es'.(En) $n)                                  /\
+  (forall k m, ((es.(rfn) k) == some m) = ((es'.(rfn) $k) == some $m)) /\
+  (if lab is w x i then es'.(En) $(es'.(n)) = W x i
+   else is_read (es'.(En) $(es'.(n))))                                  /\
   (if place == 0 then forall n, ~~ n <=[es] $place
    else (place < es'.(n)) && $place <=[es] $(es'.(n))).
 
-Notation "es '-[' n , l ']-->' es' " := (evstruct_rel es es' l n) (at level 0).*)
+Notation "es '-[' n , l ']-->' es' " := (evstruct_rel es es' l n) (at level 0).
 
 End nat_event_structure.
