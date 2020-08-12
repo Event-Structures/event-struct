e08d3567f79a0e214c602b00af79bd6a2a031739
volodeyka <vovaglad00@gmail.com>
2020-08-12 12:03:39 +0300

    sperate definitions of rf and po from their properties

--- EventStructure.v
+++ EventStructure.v
@@ -63,20 +63,27 @@ Inductive tr_label :=
 Definition ord_of {n} {Q : 'I_n -> Prop} (k : {m : 'I_n | Q m}) :=
    match k with exist m _ => m end.
 
+Definition sig_of_ord {T} (p : pred T) (x : T) : option {x : T | p x}.
+case H : (p x); last by exact: None. by exact: some (exist _ x H). Defined.
+Print simpl_rel.
+Definition rel_of {T : finType} (f : T -> option T) :=
+  connect [rel x y | if f y is some z then (z == x) else false].
+
+
 Structure evstruct := Pack {
-  n  : nat;
-  E  : 'I_n -> ev_label;
-  po : forall (m : 'I_n), {k : 'I_n | k < m}; (* Structure {tval : 'I_n, _ : tval < m} *)
-  rf : forall k, 
-  let l := E k in let x := location l in let i := value l in
-  let rpo := connect [rel x y | x == ord_of (po y)] in
-   is_read l -> {m : 'I_n | 
-   (is_write_to x i (E m)) && (* we read frim Write *)
-    ~~ (rpo k m)           && (* acyclic po with rf *)
-    ~~[exists x, [exists y, (rpo x m)                        &&      (* |   *)
-                            (rpo y k)                        &&      (* |   *)
-                            (ord_of (po x) == ord_of (po y)) &&      (* | -> rf can't match two conflict events *)
-                            (x != y) && (tid (E x) == tid (E y))]]}; (* |   *)
+  n            : nat;
+  E            : 'I_n -> ev_label;
+  po           : 'I_n -> option 'I_n;
+  acycl_po     : forall n, po n < n;
+  rf           : {k : 'I_n | is_read (E k)} -> 'I_n;
+  rf_codom     : forall (k : {k : 'I_n | is_read (E k)}),
+                 let l := E (ord_of k) in let x := location l in let i := value l in is_write_to x i l;
+  rf_po_acycl  : forall k,  ~~ (rel_of po) (ord_of k) (rf k);
+  rf_non_confl : forall k, let m := ord_of k in let l := rf k in
+   ~~[exists x, [exists y, ((rel_of po) x m)                &&      (* |   *)
+                           ((rel_of po) y l)                &&      (* |   *)
+                           (po x == po y) && (po x)         &&      (* | -> rf can't match two conflict events *)
+                           (x != y) && (tid (E x) == tid (E y))]];  (* |   *)
 }.
 
 Arguments po {_}.
@@ -88,11 +95,11 @@ Section Cause_Conflict.
 Variables (e : evstruct).
 Implicit Type n m : 'I_(n e).
 
-Definition pre_rpo m n := n == ord_of (po m).
-Definition rpo := connect pre_rpo.
+Definition is_readn n := is_read (E n).
 
-Definition rrf n m := n == ord_of (po m).
-Definition cause := connect [rel n m | rrf n m || rpo n m].
+
+Definition rrf := [rel n m | if sig_of_ord is_readn n is some x then m == rf x else false].
+Definition cause := connect [rel m n | rrf n m || (rel_of po) n m].
 
 Lemma irrefl_cause: irreflexive cause.
 Proof. Admitted.
@@ -104,7 +111,7 @@ Lemma anti_cause: antisymmetric cause.
 Proof. Admitted.
 
 
-Definition pre_conflict n m := (n != m) && (tid (E n) == tid (E m)) && (ord_of (po n) == ord_of (po m)).
+Definition pre_conflict n m := (n != m) && (tid (E n) == tid (E m)) && (po n == po m) && (po n).
 Definition conflict n m := [exists x, [exists y, (cause x m) && (cause y n) && (pre_conflict x y)]].
 Notation "a # b" := (conflict a b) (at level 0).
 
