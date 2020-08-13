eb0cd07b1b48b0335f9577b6e91710c0dd67dc57
volodeyka <vovaglad00@gmail.com>
2020-08-13 10:42:40 +0300

    fomulations of basic properties

--- EventStructure.v
+++ EventStructure.v
@@ -74,7 +74,12 @@ Definition rel_of {T : finType} (f : T -> option T) :=
   connect [rel x y | if f y is some z then (z == x) else false].
 Definition restr {n} (f : nat -> option nat) : 'I_n -> option 'I_n := 
   fun n => if f n is some m then some [m to n] else None.
-Definition prefix {T} (s s' : seq T) : bool. Admitted.
+Fixpoint prefix {T : eqType} (s s' : seq T) : bool :=
+  match s, s with
+  | [::], _            => true
+  | h1 :: t1, h2 :: t2 => (h1 == h2) && (prefix t1 t2)
+  | _, _               => false
+  end.
 
 
 
@@ -87,9 +92,9 @@ Structure evstruct := Pack {
   po_dom       : forall m, m >= N -> po m = None;
   acycl_po     : forall n, (po n) < n;
   rf           : nat -> option nat;
+  acycl_rf     : forall n, (rf n) < n;
   rf_dom       : forall m, reflect (rf m) (is_read (E m));
   rf_codom     : forall n k, is_read (E n) -> rf n = some k -> write_read (E k) (E n);
-  rf_po_acycl  : forall k l : 'I_N, (restr rf) k = some l -> ~~ (rel_of (restr po)) k l;
   rf_non_confl : forall k l : 'I_N, (restr rf) k = some l -> 
    ~~[exists x, [exists y, ((rel_of (restr po)) x k)        &&      (* |   *)
                            ((rel_of (restr po)) y l)        &&      (* |   *)
@@ -97,57 +102,82 @@ Structure evstruct := Pack {
                            (x != y) && (tid (E x) == tid (E y))]];  (* |   *)
 }.
 
-Arguments po {_}.
-Arguments rf {_}.
-Arguments E  {_}.
-
 (* derive cause conflict and properties ... *)
 Section Cause_Conflict.
 Variables (e : evstruct).
-Implicit Types (n m : 'I_(N e)) (x : val) (i : var).
+Implicit Types (x : val) (i : var).
 Notation N := (N e).
+Notation po := (po e).
+Notation E := (E e).
+Notation rf := (rf e).
 
-(*Section adding_event.
-Variables (add_t : option nat) (l : ev_label).
+
+
+Section adding_event.
+Variables (add_t : option 'I_N) (l : ev_label).
+Hypothesis H : if add_t is some t then
+                 prefix (tid (E t)) (tid (some l))
+               else true.
 
 Definition add_po : nat -> option nat := 
-  fun l => if l == N then add_t else po l.
-
+  fun l => if l == N then if add_t is some t then some (nat_of_ord t) else None else po l.
+  
 Lemma add_po_dom k : k >= N.+1 -> add_po k = None.
 Proof. Admitted.
 
 Lemma acycl_add_po k: add_po k < k.
 Proof. Admitted.
 
-Definition radd_po := connect (rel_of (restr add_po)).
+Lemma add_po_tid n m : add_po n = some m -> prefix (tid (E m)) (tid (E n)). 
+Proof. Admitted.
 
-(* seq of all writes of (value l) to (variable x) *)
+Definition radd_po := connect (rel_of (@restr N.+1 add_po)).
+
+(* seq of all writes of (value l) to (variable x) if (is_read add_t) and [::] else *)
 Definition possible_writes : seq 'I_N.+1. Admitted.
-Definition add_E : nat -> ev_label := fun t => if t == N then @E e t else l.
+Definition add_E : nat -> option ev_label := fun t => if t == N then E t else some l.
 
-Definition seq_of_writes' := 
-  if add_t is some t then
-     [seq z <- possible_writes | ~~ (radd_po t z) &
+Definition seq_of_writes := 
+  if add_t is some t' then let t := [t' to (@ord_max N)] in
+     [seq z <- possible_writes |
      ~~[exists x, [exists y, (radd_po x t) && (radd_po y z) &&
                              (add_po x == add_po y) && (add_po x) &&
                              (x != y) && (tid (add_E x) == tid (add_E y))]]]
   else [::].
 
-Definition add_rf' n : {k : 'I_N.+1 | is_read (add_E k)} -> 'I_N.+1.
-case=> x.
+Definition add_rf n : nat -> option nat :=
+  fun l => if l == N then some n else rf l.
 
-Lemma : .
-Proof.
-  
-Qed.
+Fixpoint seq_of_add_rf_func (s : seq 'I_N.+1) := 
+  if s is h :: t then (add_rf h) :: seq_of_add_rf_func t else [::].
+Definition seq_of_add_fr := if (is_read (some l)) then seq_of_add_rf_func seq_of_writes else [:: rf].
+
+Section add_rf_properties.
+Variable (n : 'I_N.+1).
+Hypothesis mem_n : n \in seq_of_writes.
+Notation add_rf := (add_rf n).
+
+Lemma acyclic_add_rf: forall k, (add_rf k) < k.
+Proof. Admitted.
 
 
+Lemma add_rf_dom: forall m, reflect (add_rf m) (is_read (E m)).
+Proof. Admitted.
+
+Lemma add_rf_codom: forall l k, is_read (E l) -> add_rf l = some k -> write_read (E k) (E n).
+Proof. Admitted.
+
+Lemma add_rf_non_confl : forall k l : 'I_N, (restr add_rf) k = some l -> 
+   ~~[exists x, [exists y, ((rel_of (restr po)) x k)        &&      (* |   *)
+                           ((rel_of (restr po)) y l)        &&      (* |   *)
+                           (po x == po y) && (po x)         &&      (* | -> rf can't match two conflict events *)
+                           (x != y) && (tid (E x) == tid (E y))]]   (* |   *).
+Proof. Admitted.
+
+End add_rf_properties.
 End adding_event.
-Definition oord_of_o (on : option 'I_N) :=
-  if on is some (Ordinal k klN) then some (Ordinal (ltn_trans klN (ltnSn N))) else None.
 
-
-Definition is_readn n := is_read (@E e n).
+(*Definition is_readn n := is_read (E n).
 
 
 Definition rrf := [rel n m | if sig_of_ord is_readn n is some x then m == rf x else false].
@@ -176,7 +206,5 @@ Proof. Admitted.
 
 Lemma consist_conflict n1 n2 n3: cause n1 n2 -> n1 # n3 -> n2 # n3.
 Proof. Admitted.*)
-
-End Cause_Conflict.
 
 End prime_event_structure.
