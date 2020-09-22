9b434d2eb41b54aa23d600bf2929ca2cae995523
volodeyka <vovaglad00@gmail.com>
2020-09-22 19:49:43 +0300

    Equations removed

--- TransitionSystem.v
+++ TransitionSystem.v
@@ -1,7 +1,6 @@
 From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat seq fintype order.
 From mathcomp Require Import eqtype fingraph path. 
 From event_struct Require Import utilities EventStructure.
-From Coq Require Import Arith.
 
 Section transition_system.
 Context {val : eqType} {dv : val}.
