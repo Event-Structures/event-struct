55114ebd2862cfc0caca51273d382c82075faf0f
Vladimir Gladstein <55004725+volodeyka@users.noreply.github.com>
2020-10-29 15:26:58 +0300

    Update inhType.v
    Co-authored-by: Anton Trunov <anton.a.trunov@gmail.com>

--- inhType.v
+++ inhType.v
@@ -1,6 +1,5 @@
 From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat seq fintype order.
 From mathcomp Require Import eqtype fingraph path. 
-From Coq Require Import ssrsearch.
 From event_struct Require Import utilities.
 
 Structure inhType : Type := Inhabitant {type :> eqType; #[canonical(false)] inh : type}.
@@ -13,5 +12,4 @@ Variable (T : inhType).
 Implicit Types (x : T) (n : nat).
 
 End Extension. 
-
 
