3f5b338ba18a4af66d5c541312e54a1aeed2ad9a
Dmitrii <mikhaylovskiy.dmitriy@gmail.com>
2020-11-20 00:16:13 +0300

    Generalized transitive closure to weakly decreasing functions

--- relations.v
+++ relations.v
@@ -17,42 +17,45 @@ Section well_founded.
 Context {T : wfType}.
 
 Definition rel_of_fun (f : T -> seq T) : rel T :=
-  [rel a b | a \in f b]. (*[seq x <- f b | x != b]].*)
+  [rel a b | a \in [seq x <- f b | x != b]].
 
 Variable (f : T -> seq T).
 
-Hypothesis descend : forall x n, x \in f n -> x < n.
+Hypothesis descend : forall x n, x \in f n -> x <= n.
 
-(*Lemma filter_lt n k : k \in [seq x <- f n | x != n] -> k < n.
+Lemma filter_lt n k : k \in [seq x <- f n | x != n] -> k < n.
 Proof.
-  by rewrite mem_filter lt_neqAle => /andP [] -> /p ->.
-Qed.*)
+   by rewrite mem_filter lt_neqAle => /andP[] -> /descend ->.
+Qed.
 
 Lemma rel_sublt a b : (rel_of_fun f) a b -> a < b.
 Proof.
-  rewrite/rel_of_fun /=. exact: descend.
+  rewrite/rel_of_fun /=. exact: filter_lt.
 Qed.
 
 (* Well-founded relation closure definition *)
 Equations(noind) t_closure (n : T) : seq T by wf n (<%O : rel T) :=
-  t_closure n := (f n) ++ flatten
-                  (map 
-                      (fun x => if x \in (f n) =P true is ReflectT pf then
-                        t_closure x
-                      else [::]) 
-                      (f n)
-                  ).
+  t_closure n := 
+    [seq x <- f n | x != n] ++ flatten
+      (map 
+          (fun x => if x \in [seq x <- f n | x != n] =P true is ReflectT pf then
+            t_closure x
+          else [::]) 
+          [seq x <- f n | x != n]
+      ).
+Next Obligation.
+  exact: filter_lt.
+Qed.
 
 Definition rel_of_closure (a b : T) : bool := a \in t_closure b.
 
-Lemma cat_eq (s1 s2 s3 : seq T) : s1 = s2 <-> s3 ++ s1 = s3 ++ s2.
+Lemma ext_t_closure (x : T) (g h : forall y : T, y < x -> seq T) :
+  (forall (y : T) (p : y < x), g y p = h y p) ->
+    t_closure_functional x g = t_closure_functional x h.
 Proof.
-  elim: s3.
-  { split=> /= [-> |] //. }
-  move=> h xs IHs. split=> [/IHs |].
-  { by rewrite !cat_cons => ->. }
-  rewrite !cat_cons => /eqP.
-  by rewrite eqseq_cons IHs => /andP [] ? /eqP.
+  move=> H. rewrite /t_closure_functional. apply/eqP.
+  rewrite eqseq_cat //=. apply/andP. split=> //=. apply/eqP.
+  congr flatten. apply/eq_in_map. move=> y /filter_lt. case: eqP=> //=.
 Qed.
 
 Lemma rel_of_closure_sublt a b : rel_of_closure a b -> a < b.
@@ -60,23 +63,20 @@ Proof.
   rewrite /rel_of_closure.
   move: b a.
   apply: wf_ind => b IH a.
-  rewrite /t_closure /Subterm.FixWf Fix_eq.
-  { rewrite {1}/t_closure_functional mem_cat=> /orP [/descend //|].
-    case/flatten_mapP=> x /descend xinf.
-    case: eqP.
-    { move=> /descend /IH /apply ax. apply: lt_trans. exact: ax. done. }
-    done. }
-  move=> x g h H. rewrite /t_closure_functional. apply/eqP.
-  rewrite eqseq_cat //=. apply/andP. split=> //=. apply/eqP.
-  congr flatten. apply/eq_in_map. move=> y /descend. case: eqP=> //=.
+  rewrite /t_closure /Subterm.FixWf Fix_eq; last by apply: ext_t_closure.
+  rewrite {1}/t_closure_functional mem_cat => /orP [/filter_lt //|].
+  case/flatten_mapP=> x /filter_lt xinf.
+  case: eqP => //.
+  move=> /filter_lt /IH /apply ax. apply: lt_trans; first by apply: ax.
+  done.
 Qed.
 
 Lemma clos_trans_sublt a b : clos_trans_n1 T (rel_of_fun f) a b -> a < b.
 Proof.
   rewrite/rel_of_fun /=.
-  elim=> [x /descend // | x y xfy acx ax].
+  elim=> [x /filter_lt // | x y xfy acx ax].
   apply: lt_trans; first by apply: ax.
-  exact: descend.
+  exact: filter_lt.
 Qed. 
 
 (* Well-founded relations properties *)
@@ -86,39 +86,27 @@ Proof.
   rewrite /rel_of_closure.
   move: b a. apply: wf_ind=> b IH a.
   apply /(iffP idP).
-  { rewrite /t_closure /Subterm.FixWf Fix_eq.
-    { rewrite {1}/t_closure_functional mem_cat => /orP [].
-      { by constructor. }
-      case/flatten_mapP=> x xinf. case: eqP.
-      { move=> /descend /IH /apply H. apply /clos_trans_tn1 /t_trans.
-        { apply /clos_tn1_trans. exact: H. }
-        by constructor. }
-      done. }
-    move=> x g h H. apply/eqP.
-    rewrite eqseq_cat //=. apply/andP. split=> //=. apply/eqP.
-    congr flatten. apply/eq_in_map.
-    move=> y /descend. case: eqP=> //=. }
+  { rewrite /t_closure /Subterm.FixWf Fix_eq; last by apply: ext_t_closure.
+    rewrite {1}/t_closure_functional mem_cat => /orP [].
+    { by constructor. }
+    case/flatten_mapP=> x xinf. case: eqP.
+    { move=> /filter_lt /IH /apply H. apply /clos_trans_tn1 /t_trans.
+      { apply /clos_tn1_trans. exact: H. }
+      by constructor. }
+    done. }
   move=> closab.
   move: (clos_tn1_trans _ (fun x x0 => rel_of_fun f x x0) _ _ closab)=> ctnab.
   move: (clos_trans_tn1 _ (fun x x0 => rel_of_fun f x x0) _ _ ctnab)=> ctab.
   move: ctab IH.
   case.
   { rewrite /rel_of_fun /= => c afc IH.
-    rewrite /t_closure /Subterm.FixWf Fix_eq.
-    { by rewrite {1}/t_closure_functional mem_cat afc. }
-    move=> x g h H. apply/eqP.
-    rewrite eqseq_cat //=. apply/andP. split=> //=. apply/eqP.
-    congr flatten. apply/eq_in_map.
-    move=> y /descend. case: eqP=> //=. }
+    rewrite /t_closure /Subterm.FixWf Fix_eq; last by apply: ext_t_closure.
+    by rewrite {1}/t_closure_functional mem_cat afc. }
   move=> c d. rewrite /rel_of_fun /= => cfd ctac IHd.
-  rewrite /t_closure /Subterm.FixWf Fix_eq.
-  { rewrite {1}/t_closure_functional mem_cat. apply/orP.
-    right. apply/flatten_mapP. exists c=> //=. case: eqP=> //=.
-    move=> _. apply/IHd=> //=. exact: descend. }
-  move=> x g h H. apply/eqP.
-  rewrite eqseq_cat //=. apply/andP. split=> //=. apply/eqP.
-  congr flatten. apply/eq_in_map.
-  move=> y /descend. case: eqP=> //=.
+  rewrite /t_closure /Subterm.FixWf Fix_eq; last by apply: ext_t_closure.
+  rewrite {1}/t_closure_functional mem_cat. apply/orP.
+  right. apply/flatten_mapP. exists c=> //=. case: eqP=> //=.
+  move=> _. apply/IHd=> //=. exact: filter_lt.
 Qed.
 
 (* reflexive-transitive closure definition *)
@@ -155,12 +143,8 @@ Proof.
   rewrite in_cons. apply/orP.
   elim: cab => [| c d cfd crac [/eqP -> | ac]]; first by left.
   { right. move: cfd. rewrite /rel_of_fun /=.
-    rewrite /t_closure /Subterm.FixWf Fix_eq.
-    { by rewrite {1}/t_closure_functional mem_cat => ->. }
-  move=> x g h H. apply/eqP.
-  rewrite eqseq_cat //=. apply/andP. split=> //=. apply/eqP.
-  congr flatten. apply/eq_in_map.
-  move=> y /descend. case: eqP=> //=. }
+    rewrite /t_closure /Subterm.FixWf Fix_eq; last by apply: ext_t_closure.
+    by rewrite {1}/t_closure_functional mem_cat => ->. }
   right. move: (closureP a c)=> refl. move: (refl ac)=> cac.
   apply /(closureP a d) /clos_trans_tn1 /t_trans.
   { apply /clos_tn1_trans. exact: cac. }
