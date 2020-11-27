a7fea0a52d8f9683f5a0d47d04e9e50a48885c9e
volodeyka <vovaglad00@gmail.com>
2020-11-27 17:24:52 +0300

    identType changed

--- ident.v
+++ ident.v
@@ -1,17 +1,26 @@
 From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq path.
+From mathcomp Require Import order choice finmap.
+From Coq Require Import ssrsearch.
+From event_struct Require Import wftype utilities.
 
 (******************************************************************************)
 (* This file contains the definitions of:                                     *)
-(*   identType == interface for inhabited types with decidable equality       *)
-(*                equipped with a function producing fresh values,            *)
-(*                e.g. identifiers.                                           *)
+(*   identType d == interface for inhabited types with decidable equality     *)
+(*               equipped with a monotone (w.r.t some wellfounded strict      *)
+(*               relation) function                                           *)
 (*      fresh id == a fresh identifier coming after id.                       *)
 (*        ident0 == an initial identifier.                                    *)
+(*      fresh_seq s == fresh h, where h is the head of s, or fresh ident0 if  *)
+(*                  s is empty                                                *)
 (*      nfresh n == a sequence of size n of fresh identifiers                 *)
 (*                  starting with ident0.                                     *)
 (* This file also contains definitions of a Canonical identType instance for  *)
 (* nat.                                                                       *)
 (******************************************************************************)
+
+Import Order.LTheory.
+Open Scope order_scope.
+Open Scope fset_scope.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -21,48 +30,57 @@ Module IdentType.
 
 Section ClassDef.
 
-Record mixin_of (T0 : Type) (b : Equality.class_of T0)
-                (T := Equality.Pack b) := Mixin {
+Record mixin_of T0 (b : WellFounded.class_of T0)
+  (T := WellFounded.Pack tt b) := Mixin {
   fresh : T -> T;
   ident0 : T;
-  _ : forall x n, uniq (traject fresh x n);
+  _ : forall x, x < fresh x;
 }.
 
 Set Primitive Projections.
 Record class_of (T : Type) := Class {
-  base  : Equality.class_of T;
+  base  : WellFounded.class_of T;
   mixin : mixin_of base;
 }.
 Unset Primitive Projections.
 
-Local Coercion base : class_of >-> Equality.class_of.
+Local Coercion base : class_of >-> WellFounded.class_of.
 
-Structure type := Pack { sort; _ : class_of sort }.
+Structure type (disp : unit) := Pack { sort; _ : class_of sort }.
 
 Local Coercion sort : type >-> Sortclass.
 
-Variables (T : Type) (cT : type).
+Variables (T : Type) (disp : unit) (cT : (type disp)).
 
 Definition class := let: Pack _ c as cT' := cT return class_of cT' in c.
-Definition clone c of phant_id class c := @Pack T c.
+Definition clone c of phant_id class c := @Pack disp T c.
 
 Definition pack :=
-  fun bT b & phant_id (Equality.class bT) b =>
-  fun m => Pack (@Class T b m).
+  fun bT b & phant_id (@WellFounded.class disp bT) b =>
+  fun m => Pack disp (@Class T b m).
 
 (* Inheritance *)
+Definition wfType := @WellFounded.Pack disp cT class.
+Definition porderType := @Order.POrder.Pack disp cT class.
+Definition choiceType := @Choice.Pack cT class.
 Definition eqType := @Equality.Pack cT class.
 End ClassDef.
 
 Module Exports.
-Coercion base : class_of >-> Equality.class_of.
+Coercion base : class_of >-> WellFounded.class_of.
 Coercion sort : type >-> Sortclass.
+Coercion wfType : type >-> WellFounded.type.
+Coercion porderType : type >-> Order.POrder.type.
+Coercion choiceType : type >-> Choice.type.
 Coercion eqType : type >-> Equality.type.
+Canonical wfType.
+Canonical porderType.
+Canonical choiceType.
 Canonical eqType.
 Notation identType := type.
-Definition fresh {T : identType} : T -> T := fresh (mixin (class T)).
-Definition ident0 {T : identType} : T := ident0 (mixin (class T)).
-Notation IdentType T m := (@pack T _ _ id m).
+Definition fresh {disp} {T : identType disp} : T -> T := fresh (mixin (class T)).
+Definition ident0 {disp} {T : identType disp} : T := ident0 (mixin (class T)).
+Notation IdentType disp T m := (@pack T disp _ _ id m).
 Notation "[ 'identType' 'of' T 'for' cT ]" := (@clone T cT _ id)
   (at level 0, format "[ 'identType'  'of'  T  'for'  cT ]") : form_scope.
 Notation "[ 'identType' 'of' T ]" := [identType of T for _]
@@ -73,30 +91,66 @@ End IdentType.
 Export IdentType.Exports.
 
 Section IdentTheory.
-Context {T : identType}.
+Context {disp} {T : identType disp}.
+Local Notation ident0 := (@ident0 disp T).
+Local Notation fresh := (@fresh disp T).
 
-Lemma uniq_traject (x : T) n : uniq (traject fresh x n).
-Proof. by case: T x n=>s [b []]. Qed.
+Lemma fresh_lt x : x < fresh x. Proof. by case: T x=> ? [/= ? []]. Qed.
+
+Definition fresh_seq s := fresh (head ident0 s).
+
+Section Add_Sorted.
+
+Context {s : seq T} (s_sorted : sorted (>%O) s).
+
+Lemma path_fresh_seq: path (>%O) (fresh_seq s) s.
+Proof.
+  case: s s_sorted=> //= ??->.
+  by rewrite /fresh_seq /= fresh_lt.
+Qed.
+
+Lemma fresh_seq_lt x : x \in s -> x < fresh_seq s.
+Proof.
+  move: path_fresh_seq; rewrite path_sortedE ?inE.
+  - by case/andP=> /swap ? /allP /apply.
+  exact/rev_trans/lt_trans.
+Qed.
+
+
+Lemma fresh_seq_le x: x \in (fresh_seq s :: s) -> x <= fresh_seq s.
+Proof. by rewrite ?inE=> /orP[/eqP->|/fresh_seq_lt/ltW]. Qed.
+
+End Add_Sorted.
+
+Definition nfresh n := iter n (fun s => fresh_seq s :: s) [:: ident0].
+
+Lemma nfreshS n: nfresh n.+1 = fresh_seq (nfresh n) :: (nfresh n).
+Proof. by []. Qed.
+
+Lemma fresh_seq_iter n: fresh_seq (nfresh n) = iter n.+1 fresh ident0.
+Proof. by elim: n=> //= ? ->. Qed.
+
+Lemma nfesh_le x n: x \in nfresh n.+1 -> x <= fresh_seq (nfresh n).
+Proof.
+  elim n=> //= [|? IHn]; rewrite ?inE=> /orP[/eqP-> //|].
+  - move/eqP->; exact/(ltW (fresh_lt _)).
+  by rewrite {2}/fresh_seq /= => /IHn /le_trans/(_ (ltW (fresh_lt _))).
+Qed.
 
 End IdentTheory.
 
-Section Nfresh.
-Context {T : identType}.
-
-Definition nfresh (n : nat) : seq T := traject fresh ident0 n.
-
-Lemma size_nfresh n : size (nfresh n) = n.
-Proof. by rewrite /nfresh size_traject. Qed.
-
-Lemma nfresh_uniq n : uniq (nfresh n).
-Proof. exact: uniq_traject. Qed.
-
-End Nfresh.
-
+Import Order.NatOrder.
 
 Section IdentDataTypes.
 
-Definition nat_identMixin := @IdentType.Mixin _ _ succn 0 iota_uniq.
-Canonical nat_identType := Eval hnf in IdentType nat nat_identMixin.
+Definition nat_identMixin :=
+  @IdentType.Mixin nat (WellFounded.class nat_wfType) succn 0 ltnSn.
 
 End IdentDataTypes.
+
+Canonical nat_identType := 
+  Eval hnf in IdentType nat_display nat nat_identMixin.
+
+(*Eval cbv in (@nfresh nat_display nat_identType 5).*)
+
+(* Definition of wfidentType *)
