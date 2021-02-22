919dabbe4b86917dddccb98576d85ea548e01b7f
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2021-02-22 13:00:14 +0300

    utils: reconcile relation closures from vanilla Coq and relation-algebra (relation-algebra missing pieces) (#64)
    Co-authored-by: Anton Trunov <anton.a.trunov@gmail.com>

--- utilities.v
+++ utilities.v
@@ -1,7 +1,7 @@
 From Coq Require Import Lia Relations.
 From mathcomp Require Import ssreflect ssrbool ssrnat ssrfun eqtype.
 From mathcomp Require Import seq path fingraph fintype.
-From RelationAlgebra Require Import lattice boolean.
+From RelationAlgebra Require Import lattice monoid boolean rel kat_tac.
 
 (* ************************************************************************** *)
 (*     Some automation with hints and tactics                                 *)
@@ -239,21 +239,83 @@ End ReflexiveClosure.
 Notation "r ^?" := (rtc r) (left associativity, at level 5, format "r ^?"): ra_terms.
 
 (* ************************************************************************** *)
-(*     Reflexive closure for decidable relations                              *)
+(*     Reconciling relation-algebra relation closures with vanilla Coq        *)
 (* ************************************************************************** *)
 
-Lemma exists_equiv {T} {A B : T -> Prop} :
-  (forall x, A x <-> B x) -> (exists x, A x) <-> exists x, B x.
-Proof. move=> H; split=> [][] x /H ?; by exists x. Qed.
+Hint Resolve r_refl rt_refl : core.
 
-Lemma clos_reflE {T} {R : relation T} a b :
-  clos_refl T R a b <-> (a = b) \/ R a b.
+Section RelAux.
+
+Context {T : Type}.
+Implicit Types (R : relation T) (r : rel T).
+
+(* TODO: refactor and rename once we'll have 
+ * reflexive closure in relation-algebra 
+ *)
+Lemma clos_reflE R :
+  clos_refl T R ≡ 1 ⊔ (R : hrel T T).
 Proof.
-  split.
-  { case; first by right. by left. }
-  case=> [->|]; first exact: r_refl. exact: r_step.
+  split; first by case; [right | left].
+  case=> [->|]; first exact: r_refl; exact: r_step.
 Qed.
 
+(* TODO: consider to reformulate it in terms of relation-algebra 
+ * (or try to just use kat tactics inplace) 
+ *)
+Lemma clos_t_clos_rt R x y :
+  clos_trans T R x y -> clos_refl_trans T R x y.
+Proof.
+  elim=> [|???? H ??]; first by constructor.
+  by apply: rt_trans H _.
+Qed.
+
+Lemma clos_r_t_is_preorder R : preorder T (clos_refl T (clos_trans T R)).
+Proof.
+  apply: Build_preorder=> //.
+  move=> x y z; rewrite clos_reflE /=.
+  case=> [-> //|xy]; case=> [<-|yz]; right=> //.
+  apply: t_trans xy yz.
+Qed.
+
+(* TODO: consider to replace it to `str_itr` *)
+Lemma clos_refl_transE R :
+  clos_refl_trans T R ≡ clos_refl T (clos_trans T R).
+Proof.
+  move=> x y; split.
+  - elim=> [{}x {}y Rxy | {}x //| {}x {}y z _ xy _ yz].
+    - by apply/r_step/t_step.
+    by apply: preord_trans xy yz; apply: clos_r_t_is_preorder.
+  by case=> // ? /clos_t_clos_rt.
+Qed.
+
+Lemma clos_trans_1n_hrel_itr R :
+  clos_trans_1n _ R ≡ (R : hrel T T)^+.
+Proof.
+  move=> x y; split.
+  - elim=> {x y} [x y | x z y] Rxy; first by exists y=> //; exists O.
+    by move=> ? [z' H' [n it]]; exists z=> //; exists n.+1, z'.
+  case=> z xz [n]; elim: n x z xz => [x z xz <-| n IHn x z xz /=].
+  - by apply/t1n_step.
+  case=> w zw /IHn - /(_ z zw) ct_zy.
+  by apply: Relation_Operators.t1n_trans xz ct_zy.
+Qed.
+
+Lemma clos_trans_hrel_itr R :
+  clos_trans _ R ≡ (R : hrel T T)^+.
+Proof.
+  move=> x y; rewrite clos_trans_t1n_iff.
+  by apply: clos_trans_1n_hrel_itr.
+Qed.
+
+Lemma clos_refl_trans_hrel_str R : 
+  clos_refl_trans _ R ≡ (R : hrel T T)^*. 
+Proof. by rewrite str_itr clos_refl_transE clos_reflE clos_trans_hrel_itr. Qed.
+
+End RelAux.
+
+Lemma exists_equiv {T} {A B : T -> Prop} :
+  (forall x, A x <-> B x) -> (exists x, A x) <-> (exists x, B x).
+Proof. move=> H; split=> [][] x /H ?; by exists x. Qed.
 
 Inductive and6 (P1 P2 P3 P4 P5 P6 : Prop) : Prop :=
   And6 of P1 & P2 & P3 & P4 & P5 & P6.
