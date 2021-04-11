824313efb1b31a6fc0b4bdf1c9f087338069f62d
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2021-04-11 16:49:58 +0300

    monad: cleanup
    * add Syntax and Theory submodules
    * do not import notation for monad morphisms by default
    * use same notation `≈>` for all types of monad morphisms
    * rename `in_bind` into `bind_mon` and move it to MonadMorphism module
    * remove duplicate lemmas `ret_ndmon` and `join_ndmon`, they should be
    derivable via coercion to MonadMorphism

--- monad.v
+++ monad.v
@@ -5,65 +5,45 @@ Import Monad.
 Local Open Scope monae_scope.
 
 Module MonadMorphism.
+
 Record mixin_of (M N : monad) (η : M ~> N) := Mixin {
   _ : forall a, η a \o Ret = Ret;
   _ : forall a (m : M (M a)),
         η a (Join m) = Join (η (N a) ((M # (η a)) m))
 }.
+
 Structure type (M N : monad) := Pack
   { cpmm : M ~> N ; class : mixin_of M _ cpmm }.
-Module Exports.
+
+Module Import Exports.
 Notation monmorph := type.
 Coercion cpmm : type >-> Natural.type.
-Notation "f ≈≈> g" := (monmorph f g) (at level 1) : monae_scope.
 Notation MonadMorphism p := (Pack (Mixin p)).
 End Exports.
-End MonadMorphism.
-Export MonadMorphism.Exports.
 
-Lemma ret_mon (M N : monad) (η : M ≈≈> N) :
+(* We do not export arrow notation by default, because we have 
+ * a monad morphism class for each subclass of monads. 
+ * So instead we use the same notation for all monad morphisms. 
+ * The user can import locally the module with the notation 
+ * for the required subclass of monads.  
+ *)
+Module Import Syntax.
+Notation "f ≈> g" := (monmorph f g) (at level 51) : monae_scope.
+End Syntax. 
+
+Module Import Theory.
+Section Theory.
+
+Lemma ret_mon (M N : monad) (η : M ≈> N) :
   forall a, η a \o Ret = Ret.
 Proof. case: η => cpmm; by case. Qed.
 
-Lemma join_mon (M N : monad) (η: M ≈≈> N) :
+Lemma join_mon (M N : monad) (η: M ≈> N) :
   forall a (m : M (M a)),
     η a (Join m) = Join (η (N a) ((M # (η a)) m)).
 Proof. case: η => cpmm; by case. Qed.
 
-Module NDMonadMorphism.
-Record mixin_of (M N : nondetMonad) (η : M ≈≈> N) := Mixin {
-  _ : forall a, η a Fail = Fail;
-  _ : forall a m n, η a (Alt m n) = Alt (η a m) (η a n)
-}.
-Structure type (M N : nondetMonad) := Pack
-  { cpmm : M ≈≈> N ; class : mixin_of M _ cpmm }.
-Module Exports.
-Notation monmorph := type.
-Coercion cpmm : type >-> MonadMorphism.type.
-Notation "f ≈> g" := (monmorph f g) (at level 1) : monae_scope.
-Notation MonadMorphism p := (Pack (Mixin p)).
-End Exports.
-End NDMonadMorphism.
-Export NDMonadMorphism.Exports.
-
-Lemma ret_ndmon (M N : nondetMonad) (η : M ≈> N) :
-  forall a, η a \o Ret = Ret.
-Proof. case: η => cpmm; case: cpmm => cpmm; by case. Qed.
-
-Lemma join_ndmon (M N : nondetMonad) (η: M ≈> N) :
-  forall a (m : M (M a)),
-    η a (Join m) = Join (η (N a) ((M # (η a)) m)).
-Proof. case: η => cpmm; case: cpmm => cpmm; by case. Qed.
-
-Lemma fail_ndmon (M N : nondetMonad) (η: M ≈> N) :
-  forall a, η a Fail = Fail.
-Proof. case: η => cpmm; by case. Qed.
-
-Lemma alt_ndmon (M N : nondetMonad) (η: M ≈> N) :
-  forall a m n, η a (Alt m n) = Alt (η a m) (η a n).
-Proof. case: η => cpmm; by case. Qed.
-
-Lemma in_bind (T : eqType) (M N : monad) (η : M ≈≈> N) (m : M T) (k : T -> M T) :
+Lemma bind_mon (T : eqType) (M N : monad) (η : M ≈> N) (m : M T) (k : T -> M T) :
   η T (m >>= k) = (η T m) >>= (fun x => η T (k x)).
 Proof.
   rewrite /Bind join_mon.
@@ -75,3 +55,49 @@ Proof.
   move=> ->. rewrite -natural. done.
 Qed.
 
+End Theory.
+End Theory.
+
+End MonadMorphism.
+
+Export MonadMorphism.Exports.
+Export MonadMorphism.Theory.
+
+Module NDMonadMorphism.
+
+Record mixin_of (M N : nondetMonad) (η : monmorph M N) := Mixin {
+  _ : forall a, η a Fail = Fail;
+  _ : forall a m n, η a (Alt m n) = Alt (η a m) (η a n)
+}.
+
+Structure type (M N : nondetMonad) := Pack
+  { cpmm : monmorph M N ; class : mixin_of M _ cpmm }.
+
+Module Import Exports.
+Notation ndmonmorph := type.
+Coercion cpmm : type >-> MonadMorphism.type.
+Notation NDMonadMorphism p := (Pack (Mixin p)).
+End Exports.
+
+Module Import Syntax.
+Notation "f ≈> g" := (ndmonmorph f g) (at level 51) : monae_scope.
+End Syntax.
+
+Module Import Theory.
+Section Theory.
+
+Lemma fail_ndmon (M N : nondetMonad) (η: M ≈> N) :
+  forall a, η a Fail = Fail.
+Proof. case: η => cpmm; by case. Qed.
+
+Lemma alt_ndmon (M N : nondetMonad) (η: M ≈> N) :
+  forall a m n, η a (Alt m n) = Alt (η a m) (η a n).
+Proof. case: η => cpmm; by case. Qed.
+
+End Theory.
+End Theory.
+
+End NDMonadMorphism.
+
+Export NDMonadMorphism.Exports.
+Export NDMonadMorphism.Theory.
