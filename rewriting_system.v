0bcdbdae5ea46305fe3cb6b5bd5c4b7cc28eeb8d
Vladimir Gladstein <55004725+volodeyka@users.noreply.github.com>
2021-05-20 23:06:55 +0300

    Rewriting systems (#88)
    * fix: minor
    
    * feat: rewriting under confluence props
    
    * feat: add instances for rewriting

--- rewriting_system.v
+++ rewriting_system.v
@@ -240,7 +240,7 @@ Section SubRewriting.
 
 Local Open Scope ra_terms.
 
-Context {S : eqType} {L : Type} {p : rel.dset S}.
+Context {S L : Type} {p : rel.dset S}.
 
 Definition sub (r : hrel S S) : hrel S S := r ⊓ (p × p).
 
@@ -282,6 +282,34 @@ Proof.
 Qed.
 
 End SubRewriting.
+
+Section EqRewritng.
+
+Context {S : Type} (r1 r1' r2 r2' e e' : hrel S S).
+
+Instance dcomm_weq: Proper 
+  ((weq : relation (hrel S S)) ==> (weq : relation (hrel S S))  ==> iff) 
+  diamond_commute.
+Proof.
+  move=> ?? E1 ?? E2; split=> D ???.
+  - rewrite -(E1 _ _) -(E2 _ _)=> /D/[apply][[s]].
+    rewrite (E1 _ _) (E2 _ _); by exists s.
+  rewrite (E1 _ _) (E2 _ _)=> /D/[apply][[s]].
+  rewrite -(E1 _ _) -(E2 _ _); by exists s.
+Qed.
+
+Instance eq_confl_weq: Proper
+  ((weq : relation (hrel S S)) ==> (weq : relation (hrel S S))  ==> iff) 
+  eqv_confluent.
+Proof.
+  move=> ?? E ?? E'; split=> D ???.
+  - rewrite -?(kleene.itr_weq E _ _)=> /D/[apply][[s1 [s2]]].
+    rewrite ?(kleene.itr_weq E _ _) (E' _ _); by exists s1, s2.
+  rewrite ?(kleene.itr_weq E _ _)=> /D/[apply][[s1 [s2]]].
+  rewrite -?(kleene.itr_weq E _ _) -(E' _ _); by exists s1, s2.
+Qed.
+
+End EqRewritng.
 
 
 
