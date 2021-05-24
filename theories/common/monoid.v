3823d3418637a55fe36d72c235d92e2a40d1d114
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2021-05-25 00:13:30 +0300

    refactor(monoid): pcm --- partial commutative monoids
    Initially, we formalized partial monoids without the assumption of
    commutativity. We believed that synchronization algebras are not
    obligated to be commutativity. It seems, however, that
    the commutativity can be assumed for synchonization algebras
    and thus we can inherit SA from PCM.
    
    The new idea for SA is to split operations into call/return sites
    (a.k.a. send/receive, request/response etc).
    For example for read/write SA we will have:
    
    L := W_req(x,v)
       | W_rsp(x,v)
       | R_req(x)
       | R_rsp(x,v)
    
    Then we can define SA as follows.
      W_rsp(x,v) + R_req(x) = R_req(x) + W_rsp(x,v) = R_rsp(x,v)

--- theories/common/monoid.v
+++ theories/common/monoid.v
@@ -5,19 +5,24 @@ From eventstruct Require Import utils.
 (******************************************************************************)
 (* This file provides a theory of (homogeneous) monoids and partial monoids.  *)
 (*                                                                            *)
-(*       Monoid.m T     == a type of monoidal structures over elements        *)
+(*       Monoid.m T     == a type of monoid structure over elements           *)
 (*                         of type T. Consists of binary associative          *)
 (*                         operation and a neutral element (unit).            *) 
-(*       Monoid.mType d == a type equipped with canonical monoidal structure. *)
+(*       Monoid.mType d == a type equipped with canonical monoid structure.   *)
 (*                                                                            *)
-(*       Monoid.pm d     == a type of monoidal structures with partial        *) 
-(*                          operation over elements of type T.                *)
-(*                          Inherits from ordinary monoidal structure.        *)
-(*                          In addition, contains a orthogonality relation    *)  
-(*                          which determines pairs of elements whose          *)
-(*                          composition is defined.                           *) 
-(*       Monoid.pmType d == a type equipped with canonical partial            *)
-(*                          monoidal structure.                               *)
+(*       Monoid.cm T     == a type of commutative monoid structure over       *)
+(*                          elements of type T. Inherets from ordinary monoid.*)
+(*       Monoid.cmType d == a type equipped with canonical structure of       *)
+(*                          commutative monoid.                               *)
+(*                                                                            *)
+(*       Monoid.pcm d     == a type of commutative monoid structures          *)
+(*                           with partial operation over elements of type T.  *)
+(*                           Inherits from commutative monoid.                *)
+(*                           In addition, contains a orthogonality relation   *)  
+(*                           which determines pairs of elements whose         *)
+(*                           composition is defined.                          *) 
+(*       Monoid.pcmType d == a type equipped with canonical structure of      *)
+(*                           partial commutative monoidal structure.          *)
 (******************************************************************************)
 
 Set Implicit Arguments.
@@ -67,38 +72,36 @@ Definition clone_with disp' c of phant_id class c := @Pack disp' T c.
 
 End ClassDef.
 
-Module Exports.
+Module Import Exports.
 Coercion mixin : class_of >-> mixin_of.
 Coercion sort : type >-> Sortclass.
 End Exports.
 
-End Monoid.
-
+Module Import Types.
 Notation m     := Monoid.class_of.
 Notation mType := Monoid.type.
+End Types.
 
-Import Monoid.Exports.
-
-Module Import MonoidDef.
-Section MonoidDef.
+Module Import Def.
+Section Def.
 
 Context {disp : unit} {M : mType disp}.
 
 Definition zero : M := Monoid.zero (Monoid.class M).
 Definition plus : M -> M -> M := Monoid.plus (Monoid.class M).
 
-End MonoidDef.
-End MonoidDef.
+End Def.
+End Def.
 
 Prenex Implicits zero plus.
 
-Module Export MonoidSyntax.
+Module Import Syntax.
 Notation "\0" := (zero) : monoid_scope.
 Notation "x \+ y" := (plus x y) : monoid_scope.
-End MonoidSyntax.
+End Syntax.
 
-Module Export MonoidTheory.
-Section MonoidTheory.
+Module Export Theory.
+Section Theory.
 
 Context {disp : unit} {M : mType disp}.
 
@@ -114,19 +117,23 @@ Lemma plusm0 (x : M) :
   x \+ \0 = x. 
 Proof. by move: x; case: M=> ? [[]]. Qed.
 
-End MonoidTheory.
-End MonoidTheory.
+End Theory.
+End Theory.
 
-Module PartialMonoid.
+End Monoid.
+
+Export Monoid.Exports.
+Export Monoid.Types.
+Export Monoid.Def.
+Export Monoid.Syntax.
+Export Monoid.Theory.
+
+Module CommutativeMonoid.
 Section ClassDef. 
 
 Record mixin_of (T0 : Type) (b : Monoid.class_of T0)
                 (T := Monoid.Pack tt b) := Mixin {
-  orth : rel T;
-  _    : orth zero zero;
-  _    : forall x, orth x zero = orth zero x;
-  _    : forall x y, orth x y -> orth x zero && orth y zero;
-  _    : forall x y z, orth (plus x y) z = orth x (plus y z);
+  _  : commutative (plus : T -> T -> T);
 }.
 
 Set Primitive Projections.
@@ -136,7 +143,77 @@ Record class_of (T : Type) := Class {
 }.
 Unset Primitive Projections.
 
+Structure type (disp : unit) := Pack { sort; _ : class_of sort }.
+
 Local Coercion base : class_of >-> Monoid.class_of.
+
+Local Coercion sort : type >-> Sortclass.
+
+Variables (T : Type) (disp : unit) (cT : type disp).
+
+Definition class := let: Pack _ c as cT' := cT return class_of (sort cT') in c.
+
+Definition pack c := @Pack disp T c.
+
+Definition clone c of phant_id class c := @Pack disp T c.
+Definition clone_with disp' c of phant_id class c := @Pack disp' T c.
+
+Definition as_mType := @Monoid.Pack disp cT class.
+
+End ClassDef.
+
+Module Import Exports.
+Coercion base : class_of >-> Monoid.class_of.
+Coercion mixin : class_of >-> mixin_of.
+Coercion sort : type >-> Sortclass.
+Coercion as_mType : type >-> Monoid.type.
+Canonical as_mType.
+End Exports.
+
+Module Import Types.
+Notation cm     := CommutativeMonoid.class_of.
+Notation cmType := CommutativeMonoid.type.
+End Types.
+
+Module Export Theory.
+Section Theory.
+
+Context {disp : unit} {M : cmType disp}.
+
+Lemma plusC (x y : M) : 
+  x \+ y = y \+ x. 
+Proof. by case: M x y=> ? [[]] ? /= []. Qed.
+
+End Theory.
+End Theory.
+
+End CommutativeMonoid.
+
+Export CommutativeMonoid.Exports.
+Export CommutativeMonoid.Types.
+Export CommutativeMonoid.Theory.
+
+
+Module PartialCommutativeMonoid.
+Section ClassDef. 
+
+Record mixin_of (T0 : Type) (b : CommutativeMonoid.class_of T0)
+                (T := CommutativeMonoid.Pack tt b) := Mixin {
+  orth : rel T;
+  _    : orth zero zero;
+  _    : forall x y, orth x y = orth y x;
+  _    : forall x y, orth x y -> orth x zero;
+  _    : forall x y z, orth (plus x y) z = orth x (plus y z);
+}.
+
+Set Primitive Projections.
+Record class_of (T : Type) := Class {
+  base  : CommutativeMonoid.class_of T;
+  mixin : mixin_of base;
+}.
+Unset Primitive Projections.
+
+Local Coercion base : class_of >-> CommutativeMonoid.class_of.
 
 Structure type (disp : unit) := Pack { sort; _ : class_of sort }.
 
@@ -147,49 +224,54 @@ Variables (T : Type) (disp : unit) (cT : type disp).
 Definition class := let: Pack _ c as cT' := cT return class_of (sort cT') in c.
 
 Definition pack :=
-  fun bE b & phant_id (@Monoid.class disp bE) b =>
+  fun bE b & phant_id (@CommutativeMonoid.class disp bE) b =>
   fun m => Pack disp (@Class T b m).
 
-Definition mType := @Monoid.Pack disp cT class.
+Definition as_mType := @Monoid.Pack disp cT class.
+Definition as_cmType := @CommutativeMonoid.Pack disp cT class.
+
 End ClassDef.
 
-Module Exports.
-Coercion base : class_of >-> Monoid.class_of.
+Module Import Exports.
+Coercion base : class_of >-> CommutativeMonoid.class_of.
 Coercion mixin : class_of >-> mixin_of.
 Coercion sort : type >-> Sortclass.
-Coercion mType : type >-> Monoid.type.
-Canonical mType.
+Coercion as_mType : type >-> Monoid.type.
+Coercion as_cmType : type >-> CommutativeMonoid.type.
+Canonical as_mType.
+Canonical as_cmType.
 End Exports.
 
-End PartialMonoid.
+Module Import Types.
+Notation pcm     := PartialCommutativeMonoid.class_of.
+Notation pcmType := PartialCommutativeMonoid.type.
+End Types.
 
-Notation pm     := PartialMonoid.class_of.
-Notation pmType := PartialMonoid.type.
+Module Import Def.
+Section Def.
 
-Import PartialMonoid.Exports.
+Context {disp : unit} {M : pcmType disp}.
 
-Module Import PartialMonoidDef.
-Section PartialMonoidDef.
+Print Coercions.
 
-Context {disp : unit} {M : pmType disp}.
-
-Definition orth : rel M := PartialMonoid.orth (PartialMonoid.class M).
+Definition orth : rel M := 
+  PartialCommutativeMonoid.orth (PartialCommutativeMonoid.class M).
 
 Definition valid : pred M := fun x => orth x zero.
 
-End PartialMonoidDef.
-End PartialMonoidDef.
+End Def.
+End Def.
 
 Prenex Implicits orth valid.
 
-Module Export PartialMonoidSyntax.
+Module Export Syntax.
 Notation "x ⟂ y" := (orth x y) : monoid_scope.
-End PartialMonoidSyntax.
+End Syntax.
 
-Module Export PartialMonoidTheory.
-Section PartialMonoidTheory.
+Module Export Theory.
+Section Theory.
 
-Context {disp : unit} {M : pmType disp}.
+Context {disp : unit} {M : pcmType disp}.
 
 Lemma orth0 : 
   orth \0 (\0 : M). 
@@ -199,13 +281,16 @@ Lemma valid0 :
   valid (\0 : M). 
 Proof. by case: M=> ? [? []]. Qed.
 
-Lemma orth_sym0 (x : M) :
-  x ⟂ \0 = \0 ⟂ x.
-Proof. by move: x; case: M=> ? [? []]. Qed.
+Lemma orth_sym (x y : M) :
+  x ⟂ y = y ⟂ x.
+Proof. by move: x y; case: M=> ? [? []]. Qed.
 
 Lemma orth_valid (x y : M) : 
   x ⟂ y -> valid x * valid y. 
-Proof. by move: x y; case: M=> ? [? [??? H ???]] /H /andP [? ?]. Qed.
+Proof. 
+  move=> /[dup]; rewrite {2}orth_sym.
+  by move: x y; case: M=> ? [? [??? H ???]] /H + /H. 
+Qed.
 
 Lemma orthA (x y z : M) : 
   (x \+ y) ⟂ z = x ⟂ (y \+ z).
@@ -215,15 +300,24 @@ Lemma orth_valid_plus (x y : M) :
   x ⟂ y = valid (x \+ y). 
 Proof. by rewrite /valid -[y in LHS]plusm0 orthA. Qed.
 
-End PartialMonoidTheory.
-End PartialMonoidTheory.
+End Theory.
+End Theory.
+
+End PartialCommutativeMonoid.
 
 End Monoid.
 
-Export Monoid.MonoidDef.
-Export Monoid.MonoidSyntax.
-Export Monoid.MonoidTheory.
+(* TODO: do not import `Def`, `Syntax`, and `Theory` modules by default (?) *)
 
-Export Monoid.PartialMonoidDef.
-Export Monoid.PartialMonoidSyntax.
-Export Monoid.PartialMonoidTheory.
+Export Monoid.Monoid.Exports.
+Export Monoid.Monoid.Def.
+Export Monoid.Monoid.Syntax.
+Export Monoid.Monoid.Theory.
+
+Export Monoid.CommutativeMonoid.Exports.
+Export Monoid.CommutativeMonoid.Theory.
+
+Export Monoid.PartialCommutativeMonoid.Exports.
+Export Monoid.PartialCommutativeMonoid.Def.
+Export Monoid.PartialCommutativeMonoid.Syntax.
+Export Monoid.PartialCommutativeMonoid.Theory.
